6168a286b605848ef12000b576298d8d69e70589
Artem <artemmeges@gmail.com>
2020-02-09 13:30:56 +0200

    add section 'soon'

--- js/main.js
+++ js/main.js
@@ -4,14 +4,24 @@ $(function(){
        dots: true,
        arrows: false,
        dotsClass: 'header-dots',
+       fade: true,
+       speed: 700,
+       autoplay: true
     });
 
     $('.header-inner__search-icon').on('click', function(){
         $('.header-inner__input').slideToggle();
-    })
+    });
 
     $('.platforms__slider').slick({
-    
+        speed: 700
+    });
+
+    $('.soon__slider').slick({
+        centerMode: true,
+        slidesToShow: 1, 
+        variableWidth: true,
+        speed: 700
     })
 
 });
